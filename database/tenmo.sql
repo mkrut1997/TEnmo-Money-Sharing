603b191cb11062d3ba1422cb781ad57eb67694d6
Michael Krut <krutmj97@gmail.com>
2022-12-14 13:24:21 -0500

    added in sql edits

--- database/tenmo.sql
+++ database/tenmo.sql
@@ -33,5 +33,28 @@ CREATE TABLE account (
 	CONSTRAINT FK_account_tenmo_user FOREIGN KEY (user_id) REFERENCES tenmo_user (user_id)
 );
 
+CREATE SEQUENCE seq_transaction_id
+  INCREMENT BY 1
+  START WITH 3001
+  NO MAXVALUE;
+
+CREATE TABLE transaction(
+	transaction_id int NOT NULL DEFAULT nextval('seq_transaction_id'),
+	from_user_id int NOT NULL,
+	to_user_id int NOT NULL,
+	amount numeric(13,2) NOT NULL,
+	CONSTRAINT PK_transaction PRIMARY KEY (transaction_id),
+	CONSTRAINT FK_transaction_from_user FOREIGN KEY (from_user_id) REFERENCES tenmo_user (user_id),
+	CONSTRAINT FK_transaction_to_user FOREIGN KEY (to_user_id) REFERENCES tenmo_user (user_id),
+	CONSTRAINT CHK_amount CHECK (amount > 0),
+	CONSTRAINT CHK_users CHECK (from_user_id != to_user_id)
+	
+);
+
 
 COMMIT;
+
+
+
+
+
